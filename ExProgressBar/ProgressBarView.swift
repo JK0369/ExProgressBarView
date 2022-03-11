de61d1a53e793825c6df18d813e0a330a9d79651
jake <palatable7@naver.com>
2022-03-12 01:37:36 +0900

    Implement progressBarView

---
+++ ExProgressBar/ProgressBarView.swift
@@ -0,0 +1,46 @@
+//
+//  ProgressBarView.swift
+//  ExProgressBar
+//
+//  Created by 김종권 on 2022/03/12.
+//
+
+import UIKit
+import SnapKit
+
+final class ProgressBarView: UIView {
+  var ratio: CGFloat = 0.0 {
+    didSet {
+      self.isHidden = !self.ratio.isLess(than: 1.0)
+      
+      self.progressBarView.snp.remakeConstraints {
+        $0.top.bottom.equalTo(self.safeAreaLayoutGuide)
+        $0.width.equalToSuperview().multipliedBy(self.ratio)
+      }
+      
+      UIView.animate(
+        withDuration: 0.5,
+        delay: 0,
+        options: .curveEaseInOut, // In과 Out 부분을 천천히하라는 의미 (나머지인 중간 부분은 빠르게 진행)
+        animations: self.layoutIfNeeded, // autolayout을 사용할때 사용
+        completion: nil
+      )
+    }
+  }
+  
+  private let progressBarView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .systemBlue
+    return view
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.isMultipleTouchEnabled = false
+    self.backgroundColor = .systemGray
+    self.addSubview(self.progressBarView)
+  }
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+}
--- ExProgressBar/ViewController.swift
+++ ExProgressBar/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+  private let progressBarView = ProgressBarView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    self.title = "ProgressBar 테스트"
+    self.view.addSubview(self.progressBarView)
+    self.progressBarView.snp.makeConstraints {
+      $0.left.top.right.equalTo(self.view.safeAreaLayoutGuide)
+      $0.height.equalTo(5)
+    }
+    
+    var timer = 0.0
+    let finish = 10.0
+    Timer.scheduledTimer(
+      withTimeInterval: 1,
+      repeats: true) { _ in
+        timer += 1
+        self.progressBarView.ratio = timer / finish
+      }
   }
-
-
 }
-
